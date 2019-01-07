0f8b61e92445f16baa9ce7d0583ed29b7023ea3f
John Doe <zzgigi2003@163.com>
2019-01-07 15:37:39 +0800

    更新测试用例

--- test/test.sh
+++ test/test.sh
@@ -11,3 +11,10 @@ curl -XPOST http://127.0.0.1:8080/kafka/topic1/upload -H "Content-Type: multipar
 echo ""
 
 curl -XPOST http://127.0.0.1:8080/kafka/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol.gz"
+echo ""
+
+curl -XPOST http://127.0.0.1:8080/kafka/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@t.log.gz"
+echo ""
+
+curl -XPOST http://127.0.0.1:8080/kafka/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@t.log"
+echo ""

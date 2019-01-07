3d97b2ea91daca4f6fd38abcb65f06549e65718a
John Doe <zzgigi2003@163.com>
2019-01-07 13:59:17 +0800

    增加测试用例

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-set -x
+# set -x
 set -e
 
 PWD=$(cd "$(dirname "$0")"; pwd)
 
 cd $PWD
+
 curl -XPOST http://127.0.0.1:8080/kafka/topoc4/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol"
+echo ""
+
+curl -XPOST http://127.0.0.1:8080/kafka/topoc4/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol.gz"

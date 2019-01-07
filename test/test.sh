4940b888a4044edbeb987bbbd46669f1c9d470dd
John Doe <zzgigi2003@163.com>
2019-01-07 14:51:43 +0800

    update test.sh

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ PWD=$(cd "$(dirname "$0")"; pwd)
 
 cd $PWD
 
-curl -XPOST http://127.0.0.1:8080/kafka/topoc4/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol"
+curl -XPOST http://127.0.0.1:8080/kafka/topic1/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol"
 echo ""
 
-curl -XPOST http://127.0.0.1:8080/kafka/topoc4/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol.gz"
+curl -XPOST http://127.0.0.1:8080/kafka/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol.gz"

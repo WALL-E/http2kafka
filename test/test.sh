49d524dbac0a13da4a1f69146fa1c2ed80cada30
John Doe <zzgigi2003@163.com>
2019-01-09 16:11:28 +0800

    修改测试脚本

--- test/test.sh
+++ test/test.sh
@@ -10,11 +10,17 @@ cd $PWD
 curl -XPOST http://127.0.0.1:8080/http2kafka/v1/topic1/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol"
 echo ""
 
-curl -XPOST http://127.0.0.1:8080/http2kafka/v1/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol.gz"
+curl -XPOST http://127.0.0.1:8080/http2kafka/v1/http2mq/upload -H "Content-Type: multipart/form-data" -F "file=@qos.sol.gz"
 echo ""
 
-curl -XPOST http://127.0.0.1:8080/http2kafka/v1/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@t.log.gz"
+curl -XPOST http://127.0.0.1:8080/http2kafka/v1/http2mq/upload -H "Content-Type: multipart/form-data" -F "file=@t.log.gz"
 echo ""
 
-curl -XPOST http://127.0.0.1:8080/http2kafka/topic2/upload -H "Content-Type: multipart/form-data" -F "file=@t.log"
+curl -XPOST http://127.0.0.1:8080/http2kafka/http2mq/upload -H "Content-Type: multipart/form-data" -F "file=@t.log"
+echo ""
+
+curl -XPOST --data '{"name": "zhangzheng"}'  http://127.0.0.1:8080/http2kafka/v1/http2mq/send
+echo ""
+
+curl -XPOST --data '{"name": "zhangzheng"}'  http://127.0.0.1:8080/http2kafka/v1/http2mq1/send
 echo ""

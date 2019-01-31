08af302bab12383487ad3338ae162cf066e619ae
John Doe <zzgigi2003@163.com>
2019-01-31 17:43:43 +0800

    更新升级脚本

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-scp -P 4239 bin/linux_amd64/producer gmu@172.16.213.183:/apps/http2kafka
+file bin/linux_amd64/producer | grep ELF
+if test $? -ne 0 ;then
+    exit 1
+else
+    echo "Deploy to the target host"
+fi
+
+
+# kong pro
+echo ""
+echo "Pro:"
+echo "scp -P 4239 bin/linux_amd64/producer gmu@172.16.213.183:/apps/http2kafka"
+echo "scp -P 4239 bin/linux_amd64/producer gmu@172.16.213.190:/apps/http2kafka"
